b687de3f9c444a598c22769ee691cb3f3f979ff9
Galin McMahon <galinmcmahon@gmail.com>
2017-03-31 12:30:00 -0700

    format output and make it work inside div

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     var userInput = $("input#numberInput").val();
-      var numberOut = 0;
-      var ArrayIn = []
-      for (numberOut = 1; numberOut <= userInput; numberOut++) {
-        ArrayIn.push(numberOut);
-      }
-        event.preventDefault();
+    var numberOut = 1;
+    var ArrayIn = []
+    for (numberOut = 1; numberOut <= userInput; numberOut++) {
+      ArrayIn.push(numberOut);
+    }
+      event.preventDefault();
 
-      ArrayIn.forEach(function(numberOut) {
-      if (numberOut % 15 === 0) {
-        var numberOut = "ping-pong"
-      } else if (numberOut % 5 === 0) {
-        var numberOut = "pong"
-      } else if (numberOut % 3 === 0) {
-        var numberOut = "ping"
-      } else {}
-        document.write("<br>")
-        document.write(numberOut);
+    ArrayIn.forEach(function(numberOut) {
+    if (numberOut >0 && numberOut % 15 === 0) {
+      var numberOut = "ping-pong"
+    } else if (numberOut > 1 && numberOut % 5 === 0) {
+      var numberOut = "pong"
+    } else if (numberOut >1 && numberOut % 3 === 0) {
+      var numberOut = "ping"
+    } else {}
+
+    $("#numberList").append("<li>" + numberOut + "</li>");
     });
   });
 });

a0c42e7d071576af96bfa2f651d312dff63814dd
Galin McMahon <galinmcmahon@gmail.com>
2017-03-31 10:04:29 -0700

    substitute multiples of 3 and 5 with ping and pong

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,19 @@ $(document).ready(function() {
       var numberOut = 0;
       var ArrayIn = []
       for (numberOut = 1; numberOut <= userInput; numberOut++) {
+        $("#outRange").show(numberOut);
         ArrayIn.push(numberOut);
-    }
+      }
+        console.log(ArrayIn);
+        event.preventDefault();
 
-      console.log(ArrayIn);
-
-
-      event.preventDefault();
+    ArrayIn.forEach(function(numberOut) {
+      if (numberOut % 3 === 0) {
+        var numberOut = "ping"
+      } else if (numberOut % 5 === 0) {
+        var numberOut = "pong"
+      } else{}
+        console.log(numberOut);
+    });
   });
 });

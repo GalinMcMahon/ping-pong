e41ad6462535488f78f04cea5994330b0e05e9a4
Galin McMahon <galinmcmahon@gmail.com>
2017-03-31 09:25:37 -0700

    convert inputted number to an array

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     var userInput = $("input#numberInput").val();
       var numberOut = 0;
+      var ArrayIn = []
       for (numberOut = 1; numberOut <= userInput; numberOut++) {
-      console.log(numberOut);
-
+        ArrayIn.push(numberOut);
     }
 
+      console.log(ArrayIn);
 
 
       event.preventDefault();

4b211aa0e1cd025a0c7e61f5f3f27da00b4ca091
Galin McMahon <galinmcmahon@gmail.com>
2017-03-31 09:14:46 -0700

    accept numerical input and return range from 1 to number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+  $("form#userInput").submit(function(event) {
+    var userInput = $("input#numberInput").val();
+      var numberOut = 0;
+      for (numberOut = 1; numberOut <= userInput; numberOut++) {
+      console.log(numberOut);
 
+    }
+
+
+
+      event.preventDefault();
+  });
 });

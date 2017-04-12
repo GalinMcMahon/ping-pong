441f8b48c868efcf22107017b6f07af59aa57351
Galin McMahon <galinmcmahon@gmail.com>
2017-04-11 20:09:00 -0700

    fixed js

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,31 @@ $(document).ready(function() {
     arrayIn.length = 0;
     var userInput = parseInt($("input#numberInput").val());
     for (numberOut = 1; numberOut <= userInput; numberOut++) {
-      arrayIn.push(numberOut);
-    }
-  });
-            // Function needs to be not nested
-  var runArray = (function() {
-    arrayIn.forEach(function(numberOut) {
+      var x;
       if (numberOut >0 && numberOut % 15 === 0) {
-        var numberOut = "ping-pong"
+         x = "ping-pong";
       } else if (numberOut > 1 && numberOut % 5 === 0) {
-        var numberOut = "pong"
+         x  = "pong";
       } else if (numberOut >1 && numberOut % 3 === 0) {
-        var numberOut = "ping"
-      } else {}
-            // Line below needs to be moved to the front end
-      $("#numberList").append("<li>" + numberOut + "</li>")
-    });
+         x = "ping";
+      } else {
+         x = numberOut;
+      }
+      arrayIn.push(x);
+    }
+
+    return arrayIn;
   });
+
 
 // Front-end logic:
   $("form#userInput").submit(function(event) {
-      pingGo();
       $("#numberList").empty();
       $(".panel").show();
       $("#game").show();
-      runArray(numberOut)
+      var output = pingGo();
+      for (var i=0; i<output.length; i++)
+         $("#numberList").append("<li>" + output[i] + "</li>")
       event.preventDefault();
   });
 });
